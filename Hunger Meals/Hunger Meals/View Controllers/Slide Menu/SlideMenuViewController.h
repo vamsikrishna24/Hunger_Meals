7ed579298434c15885d8fb7be60e82d754dcb77b
sivajeebattina <sivajee.battina@mtuity.com>
2016-07-14 18:30:38 +0530

    Pods and initial setup added

---
+++ Hunger Meals/Hunger Meals/View Controllers/Slide Menu/SlideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SlideMenuViewController.h
+//  Hunger Meals
+//
+//  Created by SivajeeBattina on 7/14/16.
+//  Copyright © 2016 paradigm-creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideMenuViewController : UIViewController
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue;
+@end
---
+++ Hunger Meals/Hunger Meals/View Controllers/Slide Menu/SlideMenuViewController.m
@@ -0,0 +1,29 @@
+//
+//  SlideMenuViewController.m
+//  Hunger Meals
+//
+//  Created by SivajeeBattina on 7/14/16.
+//  Copyright © 2016 paradigm-creatives. All rights reserved.
+//
+
+#import "SlideMenuViewController.h"
+
+@interface SlideMenuViewController ()
+
+@end
+
+@implementation SlideMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue { }
+
+@end
