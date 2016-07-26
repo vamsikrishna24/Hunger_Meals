50c21d5e73549add67609d9f6d8bcd14a083a8f9
vamsikrishna24 <krishna.tangudu@mtuity.com>
2016-07-26 15:01:01 +0530

    google added to pods and updated

--- Hunger Meals/Pods/Target Support Files/Pods-Hunger Meals/Pods-Hunger Meals-resources.sh
+++ Hunger Meals/Pods/Target Support Files/Pods-Hunger Meals/Pods-Hunger Meals-resources.sh
@@ -58,9 +58,11 @@ install_resource()
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "GoogleSignIn/Resources/GoogleSignIn.bundle"
   install_resource "IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "GoogleSignIn/Resources/GoogleSignIn.bundle"
   install_resource "IQKeyboardManager/IQKeyboardManager/Resources/IQKeyboardManager.bundle"
 fi
 
