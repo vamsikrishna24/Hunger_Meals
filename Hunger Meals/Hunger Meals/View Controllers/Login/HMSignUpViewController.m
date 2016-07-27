2c004afb5fa64eaa2c7972f52dc5e7a901558e8e
sivajeebattina <sivajee.battina@mtuity.com>
2016-07-27 20:19:29 +0530

    Few errors fixed and Twilio SDK added

--- Hunger Meals/Hunger Meals/View Controllers/Login/HMSignUpViewController.m
+++ Hunger Meals/Hunger Meals/View Controllers/Login/HMSignUpViewController.m
@@ -24,14 +24,37 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)mobileNumVerificationPressed:(id)sender{
+    // Use your own details here
+    
+    NSString *twilioSID = @"AC3cfad426abd97d934a545f9595bdcad0";
+    NSString *twilioSecret = @"ec688996442e481f23a9b432e2571fae";
+    NSString *fromNumber = @"%2B16467834051";
+    NSString *toNumber = @"%2B919703772035";
+    NSString *message = @"Hey";
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://%@:%@@api.twilio.com/2010-04-01/Accounts/%@/SMS/Messages", twilioSID,twilioSecret,twilioSID];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = [[NSString stringWithFormat:@"From=%@&To=%@&Body=%@", fromNumber, toNumber, message] dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURLSession *sharedSession = [NSURLSession sharedSession];
+    NSURLSessionTask *sessionTask = [sharedSession dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        //do somethign here
+        if (error == nil) {
+            NSString* newStr = [[NSString alloc] initWithData:data
+                                                     encoding:NSUTF8StringEncoding];
+            NSLog(@"called successfully");
+        }
+        else{
+            
+        }
+    }];
+    
+    [sessionTask resume];
+    
 }
-*/
+
 
 @end
