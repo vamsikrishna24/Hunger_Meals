a956d8e3b0420f7ae5b191d2801d104e8269947f
vamsikrishna24 <krishna.tangudu@mtuity.com>
2016-07-28 17:13:50 +0530

    committing changes after integrating PayUMoney

---
+++ Hunger Meals/Hunger Meals/View Controllers/PaymentPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaymentGateway
+//
+//  Created by Suraj on 22/07/15.
+//  Copyright (c) 2015 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentPageViewController : UIViewController
+
+
+@end
+
---
+++ Hunger Meals/Hunger Meals/View Controllers/PaymentStatusViewController.h
@@ -0,0 +1,17 @@
+//
+//  PaymentStatusViewController.h
+//  PaymentGateway
+//
+//  Created by Suraj on 30/07/15.
+//  Copyright (c) 2015 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentStatusViewController : UIViewController {
+    
+}
+
+@property (nonatomic, strong) NSMutableDictionary *mutDictTransactionDetails;
+
+@end
