cf78b6ab9e3d33e4437f38ff79810dd3feb4144a
Vamsi <tangudu.vamsikrishna@gmail.com>
2016-08-26 13:33:15 +0530

    Committing the changes in categories

--- Hunger Meals/Hunger Meals/View Controllers/AddonsViewController.m
+++ Hunger Meals/Hunger Meals/View Controllers/AddonsViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "AddonsViewController.h"
+#import "MealsTableViewCell.h"
 
-@interface AddonsViewController ()
+@interface AddonsViewController (){
+    BOOL isCellExpanded;
+    NSInteger tableViewHeight;
+}
+
+@property(nonatomic, strong) NSMutableArray *dishImagesArray;
+
+
 
 @end
 
@@ -16,22 +24,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.dishImagesArray = [[NSMutableArray alloc]initWithObjects:@"dish1",@"dish2",@"dish3",@"dish4",@"dish5",@"dish6",@"dish7",@"dish8",@"dish9",@"dish10",@"dish11",@"dish12",@"dish13",@"dish14",@"dish15",@"dish16", nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    isCellExpanded = NO;
+    tableViewHeight = 210;
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Table view data source
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.dishImagesArray count];
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"MealsCellIdentifier";
+    MealsTableViewCell *cell = (MealsTableViewCell*)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    NSString *imageName = [NSString stringWithFormat:@"Dish_Images/%@.jpg",self.dishImagesArray[indexPath.row]];
+    cell.itemImageView.image = [UIImage imageNamed:imageName];
+    cell.titleLabel.text = [NSString stringWithFormat:@"Hot and Spicy Food Item  %ld",(long)indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    MealsTableViewCell *cell = (MealsTableViewCell *)[tableView cellForRowAtIndexPath:indexPath];
+    if (isCellExpanded) {
+        isCellExpanded = NO;
+    }
+    else{
+        isCellExpanded = YES;
+    }
+    [tableView beginUpdates];
+    [tableView endUpdates];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSIndexPath *selectedIndexPath  = [tableView indexPathForSelectedRow];
+    
+    if (indexPath == selectedIndexPath && !isCellExpanded) {
+        return 280;
+    }
+    return 210;
+}
+
 
 @end
