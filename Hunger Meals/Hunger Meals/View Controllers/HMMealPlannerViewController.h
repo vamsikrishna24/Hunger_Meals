868dde4ccbee71aea477c9cce60dd7f2131d322a
Vamsi <tangudu.vamsikrishna@gmail.com>
2016-09-20 15:11:51 +0530

    Committing the changes After updating pods by adding Jtcalendar and integration of JTcalendar  into application

---
+++ Hunger Meals/Hunger Meals/View Controllers/HMMealPlannerViewController.h
@@ -0,0 +1,19 @@
+//
+//  HMMealPlannerViewController.h
+//  Hunger Meals
+//
+//  Created by Vamsi on 20/09/16.
+//  Copyright © 2016 paradigm-creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <JTCalendar/JTCalendar.h>
+
+@interface HMMealPlannerViewController : UIViewController<JTCalendarDelegate>
+@property (weak, nonatomic) IBOutlet JTCalendarMenuView *calendarMenuView;
+@property (weak, nonatomic) IBOutlet JTHorizontalCalendarView *calendarContentView;
+@property (weak, nonatomic) IBOutlet JTCalendarWeekDayView *weekDayView;
+
+@property (strong, nonatomic) JTCalendarManager *calendarManager;
+
+@end
