a945faf0bfbf7d7311e30c727e34695d42e7ce2d
Vamsi <tangudu.vamsikrishna@gmail.com>
2016-11-23 23:42:05 +0530

    PaymentsuccessScreen

---
+++ Hunger Meals/Hunger Meals/View Controllers/HMPaymentSuccessViewController.m
@@ -0,0 +1,17 @@
+//
+//  HMPaymentSuccessViewController.m
+//  Hunger Meals
+//
+//  Created by Vamsi on 23/11/16.
+//  Copyright © 2016 paradigm-creatives. All rights reserved.
+//
+
+#import "HMPaymentSuccessViewController.h"
+
+@implementation HMPaymentSuccessViewController
+
+- (IBAction)closeAction:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+@end
