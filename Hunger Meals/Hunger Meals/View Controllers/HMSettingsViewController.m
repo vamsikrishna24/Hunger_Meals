250129ff2b2fab9ac384ef06ee81fd645a98e0d8
Vamsi <tangudu.vamsikrishna@gmail.com>
2016-11-24 01:37:12 +0530

    push

--- Hunger Meals/Hunger Meals/View Controllers/HMSettingsViewController.m
+++ Hunger Meals/Hunger Meals/View Controllers/HMSettingsViewController.m
@@ -14,6 +14,7 @@
 #import "AppDelegate.h"
 #import "UserData.h"
 #import "HMAboutUsViewController.h"
+#import "HMPaymentSuccessViewController.h"
 
 @interface HMSettingsViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *settingsTableView;
@@ -127,7 +128,7 @@ else if(indexPath.row == 2) {
     }
     if (indexPath.row == 3) {
         UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-        HMAboutUsViewController *aboutVC= [mainStoryBoard instantiateViewControllerWithIdentifier:@"AboutUsSidentifier"];
+        HMPaymentSuccessViewController *aboutVC= [mainStoryBoard instantiateViewControllerWithIdentifier:@"PaymentSuccessidentifier"];
         [self presentViewController:aboutVC animated:YES completion:nil];
     }
 }
